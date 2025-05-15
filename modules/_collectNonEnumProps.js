336f199a3c03748e90a7206c00e0abc699a4aebe
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-05-15 16:43:06 +0800

    :sparkles: add `_collectNonEnumProps` function, it's for collecting non-enumerable properties in IE

---
+++ modules/_collectNonEnumProps.js
@@ -0,0 +1,67 @@
+import { nonEnumerableProps, ObjProto } from "./_setup.js";
+import isFunction from "./isFunction.js";
+import has from "./_has.js";
+
+/**
+ * Internal helper function to create a simple lookup structure.
+ * `collectNonEnumProps` used to depend on `_.contains`, but this led to 
+ * circular imports. `emulatedSet` is a one-off solution that only works
+ * for arrays or strings.
+ * 
+ * @param {any[]} keys Input array used to emulate the set, every
+ * element in it is recorded as `true`.
+ * 
+ * @returns {{ contains: (key: any) => boolean; push: (key: any) => number}}
+ * 
+ * @example
+ * var test = emulatedSet([1, 2, 3]);
+ * 
+ * console.log(test.contains(3)); // => true
+ * console.log(test.contains(4)); // => false
+ * console.log(test.push(4));     // => 4
+ * console.log(test.contains(4)); // => true
+ */
+function emulatedSet(keys) {
+  var hash = {};
+  for (var l = keys.length, i = 0; i < l; ++i) hash[keys[i]] = true;
+  return {
+    contains: function(key) { return hash[key] === true; },
+    push: function(key) {
+      hash[key] = true;
+      return keys.push(key);
+    }
+  }
+
+  // This is a clojure, the returned object has access to variable `keys`
+}
+
+/**
+ * Internal helper function. Check `keys` for the presence of keys in IE < 9
+ * that won't be iterated by `for key in ..` and thus missed. Extends `keys`
+ * in place if needed.
+ * 
+ * @param {object} obj
+ * @param {any[]} keys
+ */
+export default function collectNonEnumProps(obj, keys) {
+  var keysSet = emulatedSet(keys);
+  var nonEnumIndex = nonEnumerableProps.length;
+  var constructor = obj.constructor;
+  var proto = (isFunction(constructor) && constructor.prototype) || ObjProto;
+
+  var prop = "constructor";
+  if (has(obj, prop) && !keysSet.contains(prop)) {
+    keysSet.push(prop);
+  }
+
+  while (nonEnumIndex--) {
+    prop = nonEnumerableProps[nonEnumIndex];
+    if (prop in obj && obj[prop] !== proto[prop] && !keysSet.contains(prop)) {
+      // - prop in obj : Property is in object or its prototype chain;
+      // - obj[prop] !== proto[prop] : Object has overwritten the property;
+      // - !keysSet.contains(prop) : Property is not in object.
+
+      keysSet.push(prop);
+    }
+  }
+}
